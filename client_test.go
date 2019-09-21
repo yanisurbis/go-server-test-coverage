d7c60aa2653e5c23c78ec17274710bbbd8eba9eb
Yanis <yanis.urbis@gmail.com>
2019-09-21 14:29:14 +0300

    Add basic test and coverage report

--- client_test.go
+++ client_test.go
@@ -1,13 +1,56 @@
 package main
 
-import "testing"
+import (
+	//"fmt"
+	//"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"encoding/json"
+	"reflect"
+)
 
-// код писать тут
-func Test(t *testing.T) {
-	got := 2
-	want := 1 + 1
+func TestFindUsers(t *testing.T) {
+	users := []User{
+		User{
+			Id:     0,
+			Name:   "yanis",
+			Age:    25,
+			About:  "my name is yanis",
+			Gender: "male",
+		},
+	}
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		result, _ := json.Marshal(users)
+		w.Write(result)
+	}))
+
+	defer ts.Close()
+
+	searchClient := &SearchClient{
+		AccessToken: "password",
+		URL:         ts.URL,
+	}
+
+	req := SearchRequest{
+		Limit:      5,
+		Offset:     0,
+		Query:      "",
+		OrderField: "",
+		OrderBy:    0,
+	}
+
+	res, err := searchClient.FindUsers(req)
+
+	if err != nil {
+		t.Errorf("Should not have error")
+	}
+	if !reflect.DeepEqual(res.Users, users) {
+		t.Errorf("Users should be equals")
+	}
+	if res.NextPage {
+		t.Errorf("There should be no next page")
 	}
 }
